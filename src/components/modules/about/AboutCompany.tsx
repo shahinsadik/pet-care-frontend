06b7770eeca22cfdba42dbd44a9ad00d6cabe397
Shahin Sadik <shahinsadik@gmail.com>
2024-11-11 20:58:28 +0600

    about our mission

--- src/components/modules/about/AboutCompany.tsx
+++ src/components/modules/about/AboutCompany.tsx
@@ -6,19 +6,10 @@ const AboutCompany = () => {
       <div className="grid lg:grid-cols-2  gap-7">
         <div>
           <h1 className="text-3xl font-bold text-teal-500">
-            SUPER TRUSTED & APPROVED PET CARE COMPANY IN BANGLADESH
+            PET CARE COMPANY IN BANGLADESH
           </h1>
           <p className="lg:mt-5 sm:mt-3 text-justify">
-            Bike Rental is your premier online destination for high-quality
-            bike. We cater to athletes and fitness enthusiasts of all levels,
-            offering a wide range of products designed to enhance performance,
-            comfort, and style. Our curated selection features top brands and
-            the latest innovations in sports gear, ensuring that our customers
-            have access to the best tools for success. At Sportify, we are
-            passionate about fostering a supportive community, providing
-            exceptional customer service, and promoting a healthy, active
-            lifestyle. Join us and experience the difference that the right
-            accessories can make in achieving your athletic goals.
+          Pet Care Tips & Stories is a go-to platform for pet owners, offering expert advice and engaging stories on pet wellness, from nutrition to grooming. It features practical guides, a supportive community, and premium content options. With interactive tools like commenting, upvoting, and advanced search, users can easily access tailored advice and connect with fellow pet lovers for a comprehensive pet care experience.
           </p>
         </div>
         <div>
@@ -33,33 +24,13 @@ const AboutCompany = () => {
       <div className="my-5">
         <h1 className="text-2xl font-bold text-teal-500">OUR MISSION</h1>
         <p className="text-justify">
-          At Sportify, our mission is to empower athletes and sports enthusiasts
-          by providing top-quality sports accessories that enhance performance,
-          comfort, and style. We are dedicated to offering a diverse range of
-          products that cater to various sports and fitness activities, ensuring
-          that our customers have access to the best gear to achieve their
-          goals. Our commitment to excellence extends beyond just our products;
-          we strive to deliver exceptional customer service, fostering a
-          community of passionate individuals who inspire and support one
-          another. By continuously innovating and staying attuned to the latest
-          trends and advancements in sports technology, Bike Rental aims to be
-          the go-to destination for all your sporting needs.
+        Our Mission at Pet Care Tips & Stories is to empower pet owners with the knowledge and resources they need to provide the best care for their furry companions. We are dedicated to offering expert advice, insightful stories, and a supportive community to help pets live healthier, happier lives. Our mission is to create a trusted platform where pet owners can learn, connect, and share their journeys, ensuring every pet receives the love and care they deserve.
         </p>
       </div>
       <div className="my-5">
         <h1 className="text-2xl font-bold text-teal-500">OUR VISION</h1>
         <p className="text-justify">
-          Our vision at Bike Rental is to become the leading online destination
-          for sports accessories, recognized for our unwavering commitment to
-          quality, innovation, and customer satisfaction. We envision a world
-          where every athlete, from beginners to professionals, has the tools
-          and resources they need to excel in their chosen sport. By building
-          strong relationships with our customers and partnering with top
-          brands, we aim to create a seamless and enjoyable shopping experience
-          that empowers individuals to push their limits and achieve their
-          personal best. Sportify aspires to inspire a healthier, more active
-          lifestyle for people of all ages, promoting the benefits of physical
-          activity and the joy of sports participation.
+        Our Vision at Pet Care Tips & Stories is to become the leading online platform for pet owners, fostering a community where every pet is understood, loved, and cared for. We envision a world where pet owners have access to expert advice, inspiring stories, and a supportive network to make informed decisions about their pets' health and happiness. Through continuous learning, innovation, and compassion, we strive to enhance the bond between pets and their owners, creating a future where pets thrive and live their best lives.
         </p>
       </div>
     </div>
