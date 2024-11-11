493fbb6d141eaa1d1f85c3d62ae2bdd9dfa43278
Shahin Sadik <shahinsadik@gmail.com>
2024-11-11 20:51:31 +0600

    about banner

--- src/components/modules/about/AboutUsBanner.tsx
+++ src/components/modules/about/AboutUsBanner.tsx
@@ -6,7 +6,7 @@ const AboutUsBanner = () => {
         style={{ backgroundImage: `url(/assets/Banner.png)` }}
       ></div>
       <div className="absolute inset-0 bg-black  opacity-80"></div>
-      <h1 className="absolute lg:text-8xl text-4xl font-extrabold text-teal-500 mt-2">
+      <h1 className="absolute lg:text-8xl text-2xl font-extrabold text-teal-500 mt-2">
         About Us
       </h1>
     </div>
